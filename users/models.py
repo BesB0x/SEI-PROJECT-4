1a74c54c9b8a0f0a26633b45f22c61e667dc8954
Thom Clegg <tomclegg25@gmail.com>
2023-04-15 10:51:42 +0100

    all relatioships done, moving to crud for atmospheres

--- users/models.py
+++ users/models.py
@@ -8,3 +8,7 @@ class User(AbstractUser):
     email = models.CharField(max_length=50)
     profile_image = models.URLField(validators=[URLValidator()])
     cover_photo = models.URLField(validators=[URLValidator()])
+    user_library = models.ManyToManyField('atmospheres.Atmosphere', related_name='put_in_library')
+
+    def __str__(self):
+        return f'{self.username}'
