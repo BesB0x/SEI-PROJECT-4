5f6b81e606589268015f7d8c615b882d0d91160d
Thom Clegg <tomclegg25@gmail.com>
2023-04-16 17:32:31 +0100

    populated helpers folder

--- client/src/helpers/auth.js
+++ client/src/helpers/auth.js
@@ -1,11 +1,60 @@
+import { Buffer } from 'buffer'
+import axios from 'axios'
 
-// Get Token
+
+const tokenName = 'ATMOS-USER-TOKEN'
 
 export const getPayload = () => {
-  const token = lo
+  const token = localStorage.getItem(tokenName)
+  if (!token) return 
+  const splitToken = token.split('.')
+  const payloadString = splitToken[1]
+  return JSON.parse(Buffer.from(payloadString, 'base64'))
 }
 
+// for Navbar changes
+export const isAuthenticated = () => {
+  const payload = getPayload()
+  if (!payload) return false
+  const currentTime = Date.now() / 1000
+  return currentTime < payload.exp
+}
 
+export const removeToken = () => {
+  localStorage.removeItem(tokenName)
+}
+
+export const getToken = () => {
+  localStorage.getItem(tokenName)
+}
+
+export const authenticated = axios.create()
+authenticated.interceptors.request.use(config => {
+  config.headers['Authorization'] = `Bearer ${getToken()}`
+  return config
+})
+
+// Return logged in user's ID
 export const loggedInUser = () => {
-  console.log('wooo')
+  const payload = getPayload()
+  if (!payload) return 
+  return payload.sub 
+}
+
+// For adding to user_library
+// export const includesUserId = (array) => {
+//   const payload = getPayload()
+//   if (!payload) return
+//   if (array) {
+//     return array.include(payload.sub)
+//   }
+// }
+
+// For Edit and Delete in User Dashboard
+export const userIsOwner = (atmos) => {
+  const payload = getPayload()
+  if (!payload) return
+  if (atmos) {
+    return payload.sub === atmos.owner_id
+  }
 }
